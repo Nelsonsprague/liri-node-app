abfe9613525388070189e53a301f12aaf32011d7
Nelson <nelsonsprague84@gmail.com>
2019-06-21 15:59:22 -0600

    initial files

---
+++ liri.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+var keys = require("./keys");
+var spotify = new Spotify(keys.spotify);
+
+// concert-this 
+// node liri.js concert-this <artist/band name here>
+
+// This will search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist and render the following information about each event to the terminal:
+
+
+// Name of the venue
+// Venue location
+// Date of the Event (use moment to format this as "MM/DD/YYYY")
+
+
+//spotify-this-song
+// This will show the following information about the song in your terminal/bash window
+
+
+// Artist(s)
+// The song's name
+// A preview link of the song from Spotify
+// The album that the song is from
+
+//movie-this
+// This will show the following information about the song in your terminal/bash window
+
+
+// Artist(s)
+// The song's name
+// A preview link of the song from Spotify
+// The album that the song is from
+
+//do-what-it-says
+// node liri.js do-what-it-says
+
+// Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+
+// It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
+// Edit the text in random.txt to test out the feature for movie-this and concert-this.
